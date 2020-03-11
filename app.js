c6643a188651eebe992e3f4a652e7a3a06216c56
MHargett23 <51856965+MHargett23@users.noreply.github.com>
2020-03-11 19:48:52 -0400

    localhost working

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.get('/notes', function (res, res) {
+  res.sendFile(path.join(__dirname + '/notes.html'));
+});
+// dont quote me on that one!
+app.get('/api/notes', function (req, res) {
+  fs.readFile(path.join(__dirname + '/db/db.json'),
+    (err, data) => {
+      if (err) throw err;
+      const ret = JSON.parse(data);
+      res.send(ret);
+    });
+});
+
+app.post('/api/notes', function (req, res) {
+  fs.readFile(path.join(__dirname + '/db/db.json'),
+    (err, data) => {
+      if (err) throw err;
+      let ret = JSON.parse(data);
+      let newNote = req.body;
+      newNote.id = uuidv4();
+      ret.push(newNote);
+      fs.writeFile(path.join(__dirname + '/db/db.json'),
+        JSON.stringify(ret),
+        (err) => {
+          if (err) throw err;
+        });
+      res.send(ret);
+    });
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+      const id = req.params.id; 
+  fs.readFile(path.join(__dirname + '/db/db.json'),
+    (err, data) => {
+      if (err) throw err;
+      const ret = JSON.parse(data);
+      const filteredArray = ret.filter(x => x.id !== id);
+      fs.writeFile(path.join(__dirname + '/db/db.json'),
+        JSON.stringify(filteredArray),
+        (err) => {
+          if (err) throw err;
+        });
+      res.send(ret);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("server listening on localhost:" + PORT)
+});
